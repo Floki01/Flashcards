7701288b740dce29b3f8555260b5f0119793f5a7
Claudioxs <c.saez15@ufromail.cl>
2022-10-28 17:49:14 -0300

    cambias de la cofig base y diseño

--- src/main/java/com/flashcard/flashcards/FlashcardsApplication.java
+++ src/main/java/com/flashcard/flashcards/FlashcardsApplication.java
@@ -1,7 +1,12 @@
 package com.flashcard.flashcards;
 
+import com.flashcard.flashcards.model.Usuario;
+import com.flashcard.flashcards.repository.UsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class FlashcardsApplication {
@@ -10,7 +15,7 @@ public class FlashcardsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlashcardsApplication.class, args);
 	}
-
+	
 	// Permite ejecutar código con interfaz funcional
 	// Se ejecuta después de que se haya creado el contexto de app y antes de que se ejecute la app principal
 	// Es como un import.sql
