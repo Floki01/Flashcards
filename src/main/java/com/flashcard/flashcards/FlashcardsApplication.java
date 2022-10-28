9d6297ba3d8512971e540e6b7d4ddadd1a14cbff
Claudioxs <c.saez15@ufromail.cl>
2022-10-28 17:09:50 -0300

    agregando html - css - img

--- src/main/java/com/flashcard/flashcards/FlashcardsApplication.java
+++ src/main/java/com/flashcard/flashcards/FlashcardsApplication.java
@@ -11,4 +11,16 @@ public class FlashcardsApplication {
 		SpringApplication.run(FlashcardsApplication.class, args);
 	}
 
+	// Permite ejecutar código con interfaz funcional
+	// Se ejecuta después de que se haya creado el contexto de app y antes de que se ejecute la app principal
+	// Es como un import.sql
+	@Bean
+	CommandLineRunner commandLineRunner(UsuarioRepository usuarios, PasswordEncoder encoder) {
+		return args -> {
+
+			usuarios.save(new Usuario("user", encoder.encode("password"), "ROLE_USER"));
+			usuarios.save(new Usuario("admin", encoder.encode("password"), "ROLE_USER,ROLE_ADMIN"));
+
+		};
+	}
 }
